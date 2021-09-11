ff9d5440a64a7da9822332925bfdf4b9e8fc0a9f
DanielR59 <daniel_pumas_59@hotmail.com>
2021-09-11 10:51:06 -0500

    Add .gitignore

---
+++ README.md
@@ -0,0 +1,18 @@
+# Proyecto antiHoss Twitch
+
+
+
+Intentamos tener actualizado el siguiente 
+[Google Sheets](https://docs.google.com/spreadsheets/d/1pr3CjSkHTXVd-i4Xa4jadwnaVCviuox8VYweWmkWOiw/edit?usp=sharing)
+con nombres del bot "Hoss00312" para generar el archivo *banlist.txt* que se usará con este [programa crado por *therealnagia*](https://github.com/therealnagia/twitchbanfromlist/releases) 
+
+
+Video explicación en nuestro canal de Youtube [Vikingo informático](https://www.youtube.com/channel/UCOItBCYDUXxe0uU7HuJaRhQ)
+
+## Requisitos
+
+```console
+pip install -r requirements.txt
+```
+
+
---
+++ test.py
@@ -0,0 +1,27 @@
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+import re
+scope = ["https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive"
+]
+creds = ServiceAccountCredentials.from_json_keyfile_name("twitchbotshossprueba1-d8418a4d4c47.json",scope)
+client = gspread.authorize(creds)
+
+DriveSheet = client.open("BotsHoss Twitch").sheet1.get_all_records(head=1) #Primer Celda es encabezado
+
+#Tomamos los datos unicamente del bot Hoss (actual atacante en Twitch)
+
+bots_names = []
+for element in DriveSheet:
+    aux = list(element.values())
+    filtro = re.search("hoss(.*)",aux[0])
+    if filtro:
+       bots_names.append(aux[0])
+
+bots_names = set(bots_names) #Eliminamos duplicados
+#Escribimos el archivo final para el script
+with open("banlist.txt", 'w') as output:
+    for row in bots_names:
+        output.write(str(row) + '\n')
+
